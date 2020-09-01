12be6facbe6a87c2a23df12828bdd21848a83987
jacobhsu <jacob.hsu.tw@gmail.com>
2020-09-01 18:10:38 +0800

    home HelloWorld

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,10 @@
+import HelloWorld from '@/components/Exchange'
+
 export default [
-    { path: '/', component: resolve=>(require(["../pages/index/index"],resolve)) },
-    { path: '/index', component: resolve=>(require(["../pages/index/index"],resolve)) },
-    { path: '/exchange', component: resolve=>(require(["../pages/exchange/exchange"],resolve)) },
-    { path: '/exchange/:pair', component: resolve=>(require(["../pages/exchange/exchange"],resolve)), name: "ExchangePair"},
-    { path: '*', component: resolve=>(require(["../pages/index/index"],resolve)) }
+    // { path: '/', component: resolve=>(require(["../pages/index/index"],resolve)) },
+    { path: '/', component: HelloWorld },
+    // { path: '/index', component: resolve=>(require(["../pages/index/index"],resolve)) },
+    // { path: '/exchange', component: resolve=>(require(["../pages/exchange/exchange"],resolve)) },
+    // { path: '/exchange/:pair', component: resolve=>(require(["../pages/exchange/exchange"],resolve)), name: "ExchangePair"},
+    // { path: '*', component: resolve=>(require(["../pages/index/index"],resolve)) }
 ];
