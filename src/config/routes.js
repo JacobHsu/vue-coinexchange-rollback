db89e0ec5db1ee82f90e2ef478c306edfc37b88d
jacobhsu <jacob.hsu.tw@gmail.com>
2020-09-07 17:56:04 +0800

    getSymbol middleware

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,6 @@
 export default [
-    { path: '/', component: resolve=>(require(["../pages/index/index"],resolve)) },
+    // { path: '/', component: resolve=>(require(["../pages/index/index"],resolve)) },
+    { path: '/', component: resolve=>(require(["../pages/exchange/exchange"],resolve)) },
     { path: '/index', component: resolve=>(require(["../pages/index/index"],resolve)) },
     { path: '/exchange', component: resolve=>(require(["../pages/exchange/exchange"],resolve)) },
     { path: '/exchange/:pair', component: resolve=>(require(["../pages/exchange/exchange"],resolve)), name: "ExchangePair"},
